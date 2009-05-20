8d49b61d542571285a37a7f79242f100cec92275
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-05-20 12:47:43 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@44 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-core/src/main/java/org/mobylet/core/util/ResourceUtils.java
+++ mobylet-core/src/main/java/org/mobylet/core/util/ResourceUtils.java
@@ -19,9 +19,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
 
 import org.mobylet.core.MobyletRuntimeException;
 
@@ -49,25 +46,6 @@ public class ResourceUtils {
 		}
 		if (classLoader == null) {
 			return null;
-		}
-		URL resourceUrl = classLoader.getResource(path);
-		if (resourceUrl != null) {
-			File f;
-			try {
-				f = new File(new URI(resourceUrl.toExternalForm()));
-			} catch (URISyntaxException e) {
-				throw new MobyletRuntimeException(
-						"[URI Syntax Exception] uri = "
-						+ resourceUrl.toExternalForm(), e);
-			}
-			if (f != null && f.exists() && f.canRead()) {
-				try {
-					return new FileInputStream(f);
-				} catch (FileNotFoundException e) {
-					throw new MobyletRuntimeException(
-							"[File Not Found in Resource] filename = " + path, e);
-				}
-			}
 		}
 		return classLoader.getResourceAsStream(path);
 	}
