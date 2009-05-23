6647b1ae4c6f00a0ac095fdaf3befa926477f8d0
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-05-23 15:31:51 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@86 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

---
+++ mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/FormTag.java
@@ -0,0 +1,9 @@
+package org.mobylet.taglibs.impl;
+
+import org.mobylet.taglibs.MobyletDynamicBodyTagSupport;
+
+public class FormTag extends MobyletDynamicBodyTagSupport {
+
+	private static final long serialVersionUID = 1452530553164922425L;
+
+}
--- mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/HiddenUidTag.java
+++ mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/HiddenUidTag.java
@@ -1,10 +1,17 @@
 package org.mobylet.taglibs.impl;
 
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+
 import org.mobylet.taglibs.MobyletDynamicSimpleTagSupport;
 
 public class HiddenUidTag extends MobyletDynamicSimpleTagSupport {
 
 	public static final String TAG = "input type=\"hidden\"";
 
+	@Override
+	public void doTag() throws JspException, IOException {
+	}
 
 }
