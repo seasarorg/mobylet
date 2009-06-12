7b0600032bc6ea42c67411b2f7226c22c7f7fdac
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-06-12 01:27:17 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@142 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/FormTag.java
+++
@@ -1,9 +0,0 @@
-package org.mobylet.taglibs.impl;
-
-import org.mobylet.taglibs.MobyletDynamicBodyTagSupport;
-
-public class FormTag extends MobyletDynamicBodyTagSupport {
-
-	private static final long serialVersionUID = 1452530553164922425L;
-
-}
--- mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/HiddenUidTag.java
+++ mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/HiddenUidTag.java
@@ -4,7 +4,11 @@ import java.io.IOException;
 
 import javax.servlet.jsp.JspException;
 
+import org.mobylet.core.Carrier;
+import org.mobylet.core.Mobylet;
+import org.mobylet.core.MobyletFactory;
 import org.mobylet.taglibs.MobyletDynamicSimpleTagSupport;
+import org.mobylet.taglibs.utils.JspWriterUtils;
 
 public class HiddenUidTag extends MobyletDynamicSimpleTagSupport {
 
@@ -12,6 +16,15 @@ public class HiddenUidTag extends MobyletDynamicSimpleTagSupport {
 
 	@Override
 	public void doTag() throws JspException, IOException {
+		Mobylet m = MobyletFactory.getInstance();
+		if (m.getCarrier() == Carrier.DOCOMO) {
+			addAttribute("name", "uid");
+			addAttribute("value", m.getUid());
+			JspWriterUtils.write(
+					getJspContext().getOut(),
+					STAG + TAG + getDynamicAttributesStringBuilder().toString() + ETAG
+					);
+		}
 		recycle();
 	}
 
