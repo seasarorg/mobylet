430499f15d0b85b9f4857ebaa1b0767d86713274
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-06-23 17:21:55 +0000

    [MOBYLET-6] URL上のカレントディレクトリ（URLパス）を取得するためのUtility追加
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@233 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

---
+++ mobylet-core/src/main/java/org/mobylet/core/util/URLUtils.java
@@ -0,0 +1,20 @@
+package org.mobylet.core.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class URLUtils {
+
+	public static String getCurrentUrl() {
+		HttpServletRequest request = RequestUtils.get();
+		if (request == null) {
+			return null;
+		}
+		String url = request.getRequestURL().toString();
+		if (request.isSecure() &&
+				url.startsWith("http:")) {
+			url = url.replace("http:", "https;");
+		}
+		return url;
+	}
+
+}
