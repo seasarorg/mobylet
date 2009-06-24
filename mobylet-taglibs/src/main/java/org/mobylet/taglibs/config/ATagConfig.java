e0cc7d237a017ba942138543e1e56ed655868b65
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-06-24 16:33:29 +0000

    [MOBYLET-9] taglibの設定を外部ファイル切り出し
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@243 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-taglibs/src/main/java/org/mobylet/taglibs/config/ATagConfig.java
+++ mobylet-taglibs/src/main/java/org/mobylet/taglibs/config/ATagConfig.java
@@ -1,6 +1,24 @@
 package org.mobylet.taglibs.config;
 
-public class ATagConfig {
+import java.util.Properties;
+
+import org.mobylet.core.util.StringUtils;
+
+public class ATagConfig extends AbstractTagConfig {
+
+
+	public static final String KEY_ISUIDQUERYREQUIRED =
+		"atag.isUidQueryRequired";
+
+	public static final String KEY_ISGUIDQUERYREQUIRED =
+		"atag.isGuidQueryRequired";
+
+	public static final String KEY_ISSESSIONCOOKIEPRIORITY =
+		"atag.isSessionCookiePriority";
+
+	public static final String KEY_ISUIDORGUIDQUERYREQUIREDINSECURE =
+		"atag.isUidOrGuidQueryRequiredInSecure";
+
 
 	protected boolean isUidQueryRequired = false;
 
@@ -11,28 +29,45 @@ public class ATagConfig {
 	protected boolean isUidOrGuidQueryRequiredInSecure = true;
 
 
-	public boolean isUidQueryRequired() {
-		return isUidQueryRequired;
+	public ATagConfig() {
+		Properties config = getConfig();
+		if (StringUtils.isNotEmpty(
+				config.getProperty(KEY_ISUIDQUERYREQUIRED))) {
+			isUidQueryRequired =
+				"TRUE".equalsIgnoreCase(config.getProperty(
+						KEY_ISUIDQUERYREQUIRED));
+		}
+		if (StringUtils.isNotEmpty(
+				config.getProperty(KEY_ISGUIDQUERYREQUIRED))) {
+			isGuidQueryRequired =
+				"TRUE".equalsIgnoreCase(config.getProperty(
+						KEY_ISGUIDQUERYREQUIRED));
+		}
+		if (StringUtils.isNotEmpty(
+				config.getProperty(KEY_ISSESSIONCOOKIEPRIORITY))) {
+			isSessionCookiePriority =
+				"TRUE".equalsIgnoreCase(config.getProperty(
+						KEY_ISSESSIONCOOKIEPRIORITY));
+		}
+		if (StringUtils.isNotEmpty(
+				config.getProperty(KEY_ISUIDORGUIDQUERYREQUIREDINSECURE))) {
+			isUidOrGuidQueryRequiredInSecure =
+				"TRUE".equalsIgnoreCase(config.getProperty(
+						KEY_ISUIDORGUIDQUERYREQUIREDINSECURE));
+		}
 	}
 
-	public void setUidQueryRequired(boolean isUidQueryRequired) {
-		this.isUidQueryRequired = isUidQueryRequired;
+
+	public boolean isUidQueryRequired() {
+		return isUidQueryRequired;
 	}
 
 	public boolean isGuidQueryRequired() {
 		return isGuidQueryRequired;
 	}
 
-	public void setGuidQueryRequired(boolean isGuidQueryRequired) {
-		this.isGuidQueryRequired = isGuidQueryRequired;
-	}
-
 	public boolean isSessionCookiePriority() {
 		return isSessionCookiePriority;
-	}
-
-	public void setSessionCookiePriority(boolean isSessionCookiePriority) {
-		this.isSessionCookiePriority = isSessionCookiePriority;
 	}
 
 	public boolean isUidOrGuidQueryRequiredInSecure() {
