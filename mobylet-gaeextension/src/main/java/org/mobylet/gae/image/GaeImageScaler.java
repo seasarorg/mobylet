688d352a467307b5a43a737950a6963cb81294df
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-07-02 17:44:19 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@274 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-gaeextension/src/main/java/org/mobylet/gae/image/GaeImageScaler.java
+++ mobylet-gaeextension/src/main/java/org/mobylet/gae/image/GaeImageScaler.java
@@ -5,7 +5,8 @@ import java.io.OutputStream;
 
 import org.mobylet.core.MobyletRuntimeException;
 import org.mobylet.core.image.ImageCodec;
-import org.mobylet.core.image.ImageDimension;
+import org.mobylet.core.image.ImageRectangle;
+import org.mobylet.core.image.ScaleType;
 import org.mobylet.core.image.impl.MobyletImageScaler;
 import org.mobylet.core.util.InputStreamUtils;
 
@@ -19,20 +20,29 @@ public class GaeImageScaler extends MobyletImageScaler {
 
 	@Override
 	public void scale(InputStream imgStream, OutputStream outImage,
-			ImageCodec type, int newWidth) {
+			ImageCodec type, int newWidth, ScaleType scaleType) {
 		try {
 			//CreateImage
 			Image image = ImagesServiceFactory.makeImage(
 					InputStreamUtils.getAllBytes(imgStream));
 			//CalcNewSize
-			ImageDimension newSize = getNewSize(
-					image.getWidth(), image.getHeight(), newWidth);
-			int width = newSize.getWidth();
-			int height = newSize.getHeight();
+			ImageRectangle rectangle = getNewRectangle(
+					image.getWidth(), image.getHeight(), newWidth, scaleType);
+			int width = rectangle.getWidth();
+			int height = rectangle.getHeight();
 			//Transform
 			ImagesService imagesService = ImagesServiceFactory.getImagesService();
+			//Resize
 			Transform transformer = ImagesServiceFactory.makeResize(width, height);
 			image = imagesService.applyTransform(transformer, image);
+			//Crop
+			if (scaleType == ScaleType.SQUARE_THUMBNAIL) {
+				transformer = ImagesServiceFactory.makeCrop(
+						rectangle.getX(), rectangle.getY(),
+						rectangle.getX() + newWidth,
+						rectangle.getY() + newWidth);
+				image = imagesService.applyTransform(transformer, image);
+			}
 			//Output
 			outImage.write(image.getImageData());
 		} catch (IOException e) {
