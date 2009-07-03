7f1bd89104d76e3802ba60c3d2c604046ea19e0c
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-07-03 02:59:07 +0000

    [MOBYLET-19] 画像変換タイプを拡張する
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@276 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-core/src/main/java/org/mobylet/core/util/ImageUtils.java
+++ mobylet-core/src/main/java/org/mobylet/core/util/ImageUtils.java
@@ -84,7 +84,7 @@ public class ImageUtils {
 
 	public static ScaleType getScaleType(String scaleTypeStr) {
 		ImageConfig config = SingletonUtils.get(ImageConfig.class);
-		ScaleType scaleType = config.getDefaultScaleType();
+		ScaleType scaleType = null;
 		if (StringUtils.isNotEmpty(scaleTypeStr)) {
 			scaleType = ScaleType.valueOf(scaleTypeStr);
 			if (scaleType == null) {
@@ -93,7 +93,7 @@ public class ImageUtils {
 		} else {
 			scaleType = config.getDefaultScaleType();
 		}
-		return config.getDefaultScaleType();
+		return scaleType;
 	}
 
 	public static ImageRectangle getNewRectangle(
