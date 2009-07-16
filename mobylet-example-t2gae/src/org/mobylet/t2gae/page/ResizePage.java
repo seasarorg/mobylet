f2c1362fa0fc9986e2f93d7fb9e88c206bc68901
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-07-16 10:32:31 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@308 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-example-t2gae/src/org/mobylet/t2gae/page/ResizePage.java
+++ mobylet-example-t2gae/src/org/mobylet/t2gae/page/ResizePage.java
@@ -4,7 +4,7 @@ import org.t2framework.commons.annotation.composite.SingletonScope;
 import org.t2framework.t2.annotation.core.Default;
 import org.t2framework.t2.annotation.core.Page;
 import org.t2framework.t2.contexts.WebContext;
-import org.t2framework.t2.navigation.Redirect;
+import org.t2framework.t2.navigation.Forward;
 import org.t2framework.t2.spi.Navigation;
 
 /**
@@ -16,6 +16,6 @@ public class ResizePage {
 
 	@Default
 	public Navigation index(WebContext context) {
-		return Redirect.to("/pages/resize2.jsp");
+		return Forward.to("/pages/resize2.jsp");
 	}
 }
