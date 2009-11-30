c36b8a7d21318625c397f9267df63f9962dab93a
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-11-30 09:01:28 +0000

    [MOBYLET-69] CSSのインライン/外部スイッチを追加
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@566 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/AnalyticsTag.java
+++ mobylet-taglibs/src/main/java/org/mobylet/taglibs/impl/AnalyticsTag.java
@@ -9,6 +9,7 @@ import org.mobylet.core.Carrier;
 import org.mobylet.core.Mobylet;
 import org.mobylet.core.MobyletFactory;
 import org.mobylet.core.analytics.AnalyticsExecutor;
+import org.mobylet.core.analytics.impl.GoogleAnalyticsConfig;
 import org.mobylet.core.util.SingletonUtils;
 import org.mobylet.taglibs.utils.JspWriterUtils;
 
@@ -16,23 +17,27 @@ public class AnalyticsTag extends SimpleTagSupport {
 
 	protected String id;
 
+	protected GoogleAnalyticsConfig config =
+		SingletonUtils.get(GoogleAnalyticsConfig.class);
 
 	@Override
 	public void doTag() throws JspException, IOException {
 		Mobylet m = MobyletFactory.getInstance();
 		if (m == null ||
 				m.getCarrier() == Carrier.OTHER) {
-			JspWriterUtils.write(
-					getJspContext().getOut(),
-					"<script type=\"text/javascript\">" +
-					"var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");" +
-					"document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));" +
-					"</script>" +
-					"<script type=\"text/javascript\">" +
-					"var pageTracker = _gat._getTracker(\"" + id + "\");" +
-					"pageTracker._trackPageview();" +
-					"</script>"
-					);
+			if (config.useJs()) {
+				JspWriterUtils.write(
+						getJspContext().getOut(),
+						"<script type=\"text/javascript\">" +
+						"var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");" +
+						"document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));" +
+						"</script>" +
+						"<script type=\"text/javascript\">" +
+						"var pageTracker = _gat._getTracker(\"" + id + "\");" +
+						"pageTracker._trackPageview();" +
+						"</script>"
+						);
+			}
 		} else {
 			AnalyticsExecutor executor =
 				SingletonUtils.get(AnalyticsExecutor.class);
