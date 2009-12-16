a520dc2484b7512fcbdebd0fecd01c169a831f9f
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2009-12-16 03:10:37 +0000

    [MOBYLET-76] Config読み込み時のエラー詳細をログに出力
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@625 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-core/src/main/java/org/mobylet/core/config/MobyletInjectionConfig.java
+++ mobylet-core/src/main/java/org/mobylet/core/config/MobyletInjectionConfig.java
@@ -35,8 +35,10 @@ public abstract class MobyletInjectionConfig {
 				}
 			} catch (Exception e) {
 				MobyletLogger logger = SingletonUtils.get(MobyletLogger.class);
-				if (logger != null && logger.isLoggable())
+				if (logger != null && logger.isLoggable()) {
 					logger.log("[mobylet] InjectionConfig [" + path + "] 参照時に例外発生 = " + e);
+					e.printStackTrace();
+				}
 			} finally {
 				InputStreamUtils.closeQuietly(inputStream);
 			}
