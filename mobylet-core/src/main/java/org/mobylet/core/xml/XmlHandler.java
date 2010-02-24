7880c91bc796b316b2925b0a26d44969f2c6581c
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2010-02-24 11:05:18 +0000

    一時的なコミット
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@660 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

---
+++ mobylet-core/src/main/java/org/mobylet/core/xml/XmlHandler.java
@@ -0,0 +1,127 @@
+package org.mobylet.core.xml;
+
+import java.util.Stack;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * 汎用XMLハンドラ.<br />
+ * <p>
+ * スキーマに依存せずXMLを解釈し、ノード化する.
+ * </p>
+ *
+ * @author Shin Takeuchi
+ *
+ */
+public class XmlHandler extends DefaultHandler {
+
+	/**
+	 * XMl解釈用Stack
+	 */
+	protected Stack<XmlNode> nodeStack;
+
+	/**
+	 * ルートノード
+	 */
+	protected XmlNode root;
+
+
+	/**
+	 * ハンドラインスタンス生成.<br />
+	 *
+	 * @param isOgnl
+	 * 		OGNL有効フラグ
+	 */
+	public XmlHandler() {
+		this.initialize();
+	}
+
+	/**
+	 * ハンドラ初期化メソッド.<br />
+	 */
+	public void initialize() {
+		this.nodeStack = null;
+		this.root = null;
+	}
+
+	/**
+	 * 解析済みのXMLルートノードインスタンスを取得する.<br />
+	 *
+	 * @return
+	 * 		解析済みXMLルートノード
+	 */
+	public XmlNode getRootNode() {
+		return this.root;
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#startDocument()
+	 */
+	@Override
+	public void startDocument() throws SAXException {
+		this.nodeStack = new Stack<XmlNode>();
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#endDocument()
+	 */
+	@Override
+	public void endDocument() throws SAXException {
+		//NOP
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+	 */
+	@Override
+	public void startElement(String uri, String localName, String name, Attributes attributes) throws SAXException {
+		XmlNode node = new XmlNode(name);
+		int attributeLength = attributes.getLength();
+		for (int i = 0; i < attributeLength; i++) {
+			node.addAttribute(attributes.getQName(i), attributes.getValue(i));
+		}
+		if (this.root == null) {
+			this.root = node;
+		} else {
+			XmlNode parent = this.nodeStack.peek();
+			parent.addChild(node);
+			node.setParent(parent);
+		}
+		this.nodeStack.push(node);
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#endElement(java.lang.String, java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void endElement(String uri, String localName, String name) throws SAXException {
+		XmlNode node = this.nodeStack.pop();
+		Object nodeValue = node.getValue();
+		if (nodeValue != null) {
+			String nodeValueString = nodeValue.toString().trim();
+			node.setValue(nodeValueString);
+		}
+	}
+
+	/*
+	 * (非 Javadoc)
+	 * @see org.xml.sax.helpers.DefaultHandler#characters(char[], int, int)
+	 */
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		XmlNode node = this.nodeStack.peek();
+		if (node.getValue() == null) {
+			node.setValue(new String(ch, start, length));
+		} else {
+			node.setValue(node.getValue() + new String(ch, start, length));
+		}
+	}
+
+}
---
+++ mobylet-core/src/main/java/org/mobylet/core/xml/XmlNode.java
@@ -0,0 +1,328 @@
+package org.mobylet.core.xml;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.mobylet.core.util.ObjectUtils;
+import org.mobylet.core.util.StringUtils;
+
+/**
+ *
+ * 解析したXMLノード情報.<br />
+ * <p>
+ * XMLノードひとつに対するノード情報を保持する.<br />
+ * このノードには単一の親ノードと複数の子ノードが紐付き、
+ * 中間ノードからどのノードでも辿ることが可能。
+ * </p>
+ *
+ * @author Shin Takeuchi
+ *
+ */
+public class XmlNode {
+
+	/**
+	 * シリアルバージョンUID
+	 */
+	private static final long serialVersionUID = -2224946250718469933L;
+
+	/**
+	 * バリュー探索パターン
+	 */
+	private static final Pattern VAL_PATTERN = Pattern.compile("[^@]+\\(.+\\)");
+
+	/**
+	 * 属性値探索パターン
+	 */
+	private static final Pattern ATT_PATTERN = Pattern.compile(".+@.+\\(.+\\)");
+
+
+	/**
+	 * ノード名（QName）
+	 */
+	protected String name;
+
+	/**
+	 * ノードバリュー
+	 */
+	protected Object value;
+
+	/**
+	 * 属性値マップ
+	 */
+	protected Map<String, String> attributes;
+
+	/**
+	 * 子ノードリスト
+	 */
+	protected List<XmlNode> children;
+
+	/**
+	 * 親ノード
+	 */
+	protected XmlNode parent;
+
+
+	/**
+	 * ノードインスタンス化.<br />
+	 *
+	 * @param name
+	 * 		ノード名
+	 */
+	public XmlNode(String name) {
+		this.name = name;
+		this.attributes = new LinkedHashMap<String, String>();
+		this.children = new ArrayList<XmlNode>();
+	}
+
+	/**
+	 * ノード名取得メソッド.<br />
+	 *
+	 * @return
+	 * 		ノード名
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * ノード名設定メソッド.<br />
+	 *
+	 * @param name
+	 * 		ノード名
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * ノードバリュー取得メソッド.<br />
+	 *
+	 * @return
+	 * 		ノードバリュー
+	 */
+	public Object getValue() {
+		return value;
+	}
+
+	/**
+	 * ノードバリュー取得メソッド.<br />
+	 *
+	 * @param <T>
+	 * 		内部キャストを行う型
+	 * @param clazz
+	 * 		内部キャストを行う型
+	 * return
+	 * 		ノードバリュー
+	 */
+	@SuppressWarnings("unchecked")
+	public <T extends Object> T getValue(Class<T> clazz) {
+		return (T)value;
+	}
+
+	/**
+	 * ノードバリュー設定メソッド.<br />
+	 *
+	 * @param value
+	 * 		ノードバリュー
+	 */
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+	/**
+	 * 属性値マップ取得メソッド.<br />
+	 *
+	 * @return
+	 * 		属性値マップ
+	 */
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
+
+	/**
+	 * 属性値マップ設定メソッド.<br />
+	 *
+	 * @param attributes
+	 * 		属性値マップ
+	 */
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
+
+	/**
+	 * 属性値追加メソッド.<br /
+	 *
+	 * @param name
+	 * 		属性名
+	 * @param value
+	 * 		属性値
+	 */
+	public void addAttribute(String name, String value) {
+		this.attributes.put(name, value);
+	}
+
+	/**
+	 * 子ノード情報取得メソッド.<br />
+	 *
+	 * @return
+	 * 		子ノードリスト
+	 */
+	public List<XmlNode> getChildren() {
+		return children;
+	}
+
+	/**
+	 * 子ノード情報取得メソッド.<br />
+	 * <p>
+	 * 指定したQNameの子ノードのみを取得する.
+	 * </p>
+	 *
+	 * @param qname
+	 * 		QName
+	 * @return
+	 * 		子ノードリスト
+	 */
+	public List<XmlNode> getChildren(String qname) {
+		List<XmlNode> childList = new ArrayList<XmlNode>();
+		for (XmlNode node : this.children) {
+			if (StringUtils.equals(node.getName(), qname)) {
+				childList.add(node);
+			}
+		}
+		return childList;
+	}
+
+	/**
+	 * 子ノード情報取得メソッド.<br />
+	 * <p>
+	 * 指定したQNameに対して、指定した値を持つ子ノード情報を取得する
+	 * </p>
+	 *
+	 * @param qname
+	 * 		QName
+	 * @param value
+	 * 		QNameに対するノードバリュー
+	 * @return
+	 * 		子ノードリスト
+	 */
+	public List<XmlNode> getChildrenByValue(String qname, Object value) {
+		List<XmlNode> childList = new ArrayList<XmlNode>();
+		for (XmlNode node : this.children) {
+			if (StringUtils.equals(node.getName(), qname) &&
+					ObjectUtils.equals(node.getValue(), value)) {
+				childList.add(node);
+			}
+		}
+		return childList;
+	}
+
+	/**
+	 * 子ノード情報取得メソッド.<br />
+	 * <p>
+	 * 指定したQNameの指定した属性名に、指定した値を持つ子ノード情報を取得する
+	 * </p>
+	 *
+	 * @param qname
+	 * 		QName
+	 * @param name
+	 * 		属性名
+	 * @param value
+	 * 		属性値
+	 * @return
+	 * 		子ノードリスト
+	 */
+	public List<XmlNode> getChildrenByAttribute(String qname, String name, String value) {
+		List<XmlNode> childList = new ArrayList<XmlNode>();
+		for (XmlNode node : this.children) {
+			if (StringUtils.equals(node.getName(), qname) &&
+					ObjectUtils.equals(node.getAttributes().get(name), value)) {
+				childList.add(node);
+			}
+		}
+		return childList;
+	}
+
+	/**
+	 * 子ノード追加メソッド.<br />
+	 *
+	 * @param child
+	 * 		子ノード
+	 */
+	public void addChild(XmlNode child) {
+		this.children.add(child);
+	}
+
+	/**
+	 * 親ノード取得メソッド.<br />
+	 *
+	 * @return
+	 * 		親ノード
+	 */
+	public XmlNode getParent() {
+		return parent;
+	}
+
+	/**
+	 * 親ノード設定メソッド.<br />
+	 *
+	 * @param parent
+	 * 		親ノード
+	 */
+	public void setParent(XmlNode parent) {
+		this.parent = parent;
+	}
+
+	/**
+	 * 独自のXPathからノードリストを取得するメソッド.<br />
+	 *
+	 * @param xpath
+	 * 		XPath（相対パス：自身のパスは含まない）
+	 * @return
+	 * 		ノードリスト
+	 */
+	public List<XmlNode> getNodeListByXPath(String xpath) {
+		List<XmlNode> nodeList = new ArrayList<XmlNode>();
+		if (xpath == null) {
+			return nodeList;
+		}
+		List<XmlNode> tempList = new ArrayList<XmlNode>();
+		nodeList.add(this);
+		String[] pathes = xpath.split("/");
+		for (String path : pathes) {
+			// エスケープされた文字を置換
+			path = path.replaceAll("_／", "/");
+			if (StringUtils.isEmpty(path)) {
+				continue;
+			}
+			tempList.clear();
+			tempList.addAll(nodeList);
+			nodeList.clear();
+			Matcher valMatcher = VAL_PATTERN.matcher(path);
+			Matcher attMatcher = ATT_PATTERN.matcher(path);
+			if (valMatcher.matches()) {
+				String qname = path.substring(0, path.indexOf("("));
+				String value = path.substring(path.indexOf("(") + 1, path.indexOf(")"));
+				for (XmlNode node : tempList) {
+					nodeList.addAll(node.getChildrenByValue(qname, value));
+				}
+			} else if (attMatcher.matches()) {
+				String qname = path.substring(0, path.indexOf("@"));
+				String name = path.substring(path.indexOf("@") + 1, path.indexOf("("));
+				String value = path.substring(path.indexOf("(") + 1, path.indexOf(")"));
+				for (XmlNode node : tempList) {
+					nodeList.addAll(node.getChildrenByAttribute(qname, name, value));
+				}
+			} else {
+				for (XmlNode node : tempList) {
+					nodeList.addAll(node.getChildren(path));
+				}
+			}
+		}
+		return nodeList;
+	}
+
+}
