5ba46620936aeecb9261f09627a3a2a860ca256e
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2010-03-03 11:59:45 +0000

    一時的にコミット
    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@665 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-core/src/main/java/org/mobylet/core/session/impl/MobyletMultiSessionManager.java
+++ mobylet-core/src/main/java/org/mobylet/core/session/impl/MobyletMultiSessionManager.java
@@ -1,8 +1,11 @@
 package org.mobylet.core.session.impl;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mobylet.core.MobyletRuntimeException;
 import org.mobylet.core.config.MobyletSessionConfig;
 import org.mobylet.core.config.MobyletSessionConfig.Parameters;
 import org.mobylet.core.holder.SessionHolder;
@@ -10,44 +13,101 @@ import org.mobylet.core.session.InvokeType;
 import org.mobylet.core.session.MobyletSessionManager;
 import org.mobylet.core.util.SerializeUtils;
 import org.mobylet.core.util.SingletonUtils;
+import org.mobylet.core.util.StringUtils;
 
 public class MobyletMultiSessionManager implements MobyletSessionManager {
 
 	protected MobyletSessionConfig config;
-	
+
 	protected SessionHolder holder;
-	
-	
+
+
 	public MobyletMultiSessionManager() {
 		config = SingletonUtils.get(MobyletSessionConfig.class);
 		holder = SingletonUtils.get(SessionHolder.class);
 	}
-	
+
 	@Override
 	public void invoke(HttpServletRequest request, HttpServletResponse response) {
 		Parameters parametersKey = config.getDistribution().getParameters();
 		String key = request.getParameter(parametersKey.getSessionKey());
 		String type = request.getParameter(parametersKey.getInvokeTypeKey());
 		String objString = request.getParameter(parametersKey.getObjectDataKey());
+		if (StringUtils.isEmpty(objString)) {
+			doEmptyResponse(response);
+			return;
+		}
 		InvokeType invokeType = InvokeType.valueOf(type);
-		switch (invokeType) {
-		case GET:
+		//GET
+		if (invokeType == InvokeType.GET) {
 			Object obj = holder.get(key, (Class<?>)SerializeUtils.deserialize(objString));
-			//TODO ここから
-		case SET:
-		case REMOVE:
-		case INVALIDATE:
-		default:
-			//NOP
+			if (obj == null) {
+				doEmptyResponse(response);
+				return;
+			}
+			byte[] objBytes = SerializeUtils.serialize2Bytes(obj);
+			response.setContentType("application/octet-stream");
+			response.setContentLength(objBytes.length);
+			try {
+				response.getOutputStream().write(SerializeUtils.serialize2Bytes(obj));
+				response.flushBuffer();
+			} catch (IOException e) {
+				throw new MobyletRuntimeException("セッションインスタンスのレスポンス送信に失敗しました", e);
+			}
+		}
+		//SET
+		else if (invokeType == InvokeType.SET) {
+			holder.set(key, SerializeUtils.deserialize(objString));
+			response.setContentType("application/octet-stream");
+			response.setContentLength(2);
+			try {
+				response.getOutputStream().write("OK".getBytes());
+				response.flushBuffer();
+			} catch (IOException e) {
+				throw new MobyletRuntimeException("セッションインスタンスのレスポンス送信に失敗しました", e);
+			}
+		}
+		//REMOVE
+		else if (invokeType == InvokeType.REMOVE) {
+			Object obj = holder.remove(key, (Class<?>)SerializeUtils.deserialize(objString));
+			if (obj == null) {
+				doEmptyResponse(response);
+				return;
+			}
+			byte[] objBytes = SerializeUtils.serialize2Bytes(obj);
+			response.setContentType("application/octet-stream");
+			response.setContentLength(objBytes.length);
+			try {
+				response.getOutputStream().write(SerializeUtils.serialize2Bytes(obj));
+				response.flushBuffer();
+			} catch (IOException e) {
+				throw new MobyletRuntimeException("セッションインスタンスのレスポンス送信に失敗しました", e);
+			}
+		}
+		//INVALIDATE
+		else if (invokeType == InvokeType.INVALIDATE) {
+			holder.invalidate(key);
+			response.setContentType("application/octet-stream");
+			response.setContentLength(2);
+			try {
+				response.getOutputStream().write("OK".getBytes());
+				response.flushBuffer();
+			} catch (IOException e) {
+				throw new MobyletRuntimeException("セッションインスタンスのレスポンス送信に失敗しました", e);
+			}
 		}
 	}
 
 	@Override
 	public boolean isManaged(HttpServletRequest request) {
-		//IPチェックが必要
+		//TODO IPチェックが必要
 		return request.getRequestURI().equals(
 				SingletonUtils.get(MobyletSessionConfig.class)
 				.getDistribution().getPath());
 	}
 
+	protected void doEmptyResponse(HttpServletResponse response) {
+		response.setContentType("application/octet-stream");
+		response.setContentLength(0);
+	}
 }
