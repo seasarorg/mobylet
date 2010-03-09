938975af6fb20b2c280e8dcfdd7932f97783687b
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2010-03-09 14:40:56 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@690 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

---
+++ mobylet-core/src/test/java/org/mobylet/core/holder/impl/MobyletSessionHolderTest.java
@@ -0,0 +1,38 @@
+package org.mobylet.core.holder.impl;
+
+import junit.framework.TestCase;
+
+import org.mobylet.core.config.MobyletSessionConfig;
+import org.mobylet.core.launcher.MobyletLauncher;
+import org.mobylet.core.util.SingletonUtils;
+
+public class MobyletSessionHolderTest extends TestCase {
+
+	public void test_sessionInvoke() {
+		//## ARRANGE ##
+		MobyletLauncher.launch();
+		MobyletSessionConfig config = SingletonUtils.get(MobyletSessionConfig.class);
+		MobyletSessionHolder holder = new MobyletSessionHolder();
+		holder.timeSpan = 10;
+
+		String key = "key";
+		String val = "ABC";
+		holder.set(key, val);
+
+		assertTrue(val.equals(holder.get(key, String.class)));
+
+		holder.remove(key, String.class);
+
+		assertTrue(holder.get(key, String.class) == null);
+
+		holder.set(key, val);
+		assertTrue(val.equals(holder.get(key, String.class)));
+		try {
+			Thread.sleep(config.getTimeout() * holder.timeSpan + 10);
+		} catch (InterruptedException e) {
+			//NOP
+		}
+		assertTrue(holder.get(key, String.class) == null);
+	}
+
+}
