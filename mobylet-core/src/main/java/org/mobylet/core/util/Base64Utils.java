605c0e84fb235ccf67d37b68504a2eee574c63fd
stakeuchi <stakeuchi@a8c47a47-7b07-41c4-b2c4-fbfeade54c8c>
2010-03-23 04:20:16 +0000

    git-svn-id: https://www.seasar.org/svn/mobylet/trunk@714 a8c47a47-7b07-41c4-b2c4-fbfeade54c8c

--- mobylet-core/src/main/java/org/mobylet/core/util/Base64Utils.java
+++ mobylet-core/src/main/java/org/mobylet/core/util/Base64Utils.java
@@ -1,147 +1,101 @@
 package org.mobylet.core.util;
 
+import java.io.ByteArrayOutputStream;
+
 public class Base64Utils {
 
-	public static String byteArrayToBase64(byte[] a) {
-		return byteArrayToBase64(a, false);
-	}
+	public static final char[] c =
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+		.toCharArray();
 
-	public static String byteArrayToAltBase64(byte[] a) {
-		return byteArrayToBase64(a, true);
-	}
-
-	private static String byteArrayToBase64(byte[] a, boolean alternate) {
-		int aLen = a.length;
-		int numFullGroups = aLen / 3;
-		int numBytesInPartialGroup = aLen - 3 * numFullGroups;
-		int resultLen = 4 * ((aLen + 2) / 3);
-		StringBuffer result = new StringBuffer(resultLen);
-		char[] intToAlpha = (alternate ? intToAltBase64 : intToBase64);
-
-		// Translate all full groups from byte array elements to Base64
-		int inCursor = 0;
-		for (int i = 0; i < numFullGroups; i++) {
-			int byte0 = a[inCursor++] & 0xff;
-			int byte1 = a[inCursor++] & 0xff;
-			int byte2 = a[inCursor++] & 0xff;
-			result.append(intToAlpha[byte0 >> 2]);
-			result.append(intToAlpha[(byte0 << 4) & 0x3f | (byte1 >> 4)]);
-			result.append(intToAlpha[(byte1 << 2) & 0x3f | (byte2 >> 6)]);
-			result.append(intToAlpha[byte2 & 0x3f]);
-		}
-
-		// Translate partial group if present
-		if (numBytesInPartialGroup != 0) {
-			int byte0 = a[inCursor++] & 0xff;
-			result.append(intToAlpha[byte0 >> 2]);
-			if (numBytesInPartialGroup == 1) {
-				result.append(intToAlpha[(byte0 << 4) & 0x3f]);
-				result.append("==");
-			} else {
-				// assert numBytesInPartialGroup == 2;
-				int byte1 = a[inCursor++] & 0xff;
-				result.append(intToAlpha[(byte0 << 4) & 0x3f | (byte1 >> 4)]);
-				result.append(intToAlpha[(byte1 << 2) & 0x3f]);
-				result.append('=');
+	public static String encode(byte[] data) {
+		StringBuilder buf = new StringBuilder(data.length * 3 / 2);
+		int end = data.length - 3;
+		int i = 0;
+		int breakNumber = 0;
+		while (i <= end) {
+			int d =
+				((((int)data[i]) & 0xFF) << 16)
+				| ((((int)data[i + 1]) & 0xFF) << 8)
+				| (((int)data[i + 2]) & 0xFF);
+			buf.append(c[(d >> 18) & 0x3F]);
+			buf.append(c[(d >> 12) & 0x3F]);
+			buf.append(c[(d >> 6) & 0x3F]);
+			buf.append(c[d & 0x3F]);
+			i += 3;
+			if (breakNumber++ >= 14) {
+				breakNumber = 0;
+				buf.append("\r\n");
 			}
 		}
-		// assert inCursor == a.length;
-		// assert result.length() == resultLen;
-		return result.toString();
+		if (i == end + 1) {
+			int d =
+				((((int)data[i]) & 0xFF) << 16)
+				| ((((int)data[i + 1]) & 0xFF) << 8);
+			buf.append(c[(d >> 18) & 0x3F]);
+			buf.append(c[(d >> 12) & 0x3F]);
+			buf.append(c[(d >> 6) & 0x3F]);
+			buf.append("=");
+		}
+		else if (i == end + 2) {
+			int d = (((int)data[i]) & 0xFF) << 16;
+			buf.append(c[(d >> 18) & 0x3F]);
+			buf.append(c[(d >> 12) & 0x3F]);
+			buf.append("==");
+		}
+		return buf.toString();
 	}
 
-	private static final char intToBase64[] = { 'A', 'B', 'C', 'D', 'E', 'F',
-			'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-			'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-			'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-			't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5',
-			'6', '7', '8', '9', '+', '/' };
-
-	private static final char intToAltBase64[] = { '!', '"', '#', '$', '%',
-			'&', '\'', '(', ')', ',', '-', '.', ':', ';', '<', '>', '@', '[',
-			']', '^', '`', '_', '{', '|', '}', '~', 'a', 'b', 'c', 'd', 'e',
-			'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-			's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4',
-			'5', '6', '7', '8', '9', '+', '?' };
-
-	public static byte[] base64ToByteArray(String s) {
-		return base64ToByteArray(s, false);
-	}
-
-	public static byte[] altBase64ToByteArray(String s) {
-		return base64ToByteArray(s, true);
-	}
-
-	private static byte[] base64ToByteArray(String s, boolean alternate) {
-		byte[] alphaToInt = (alternate ? altBase64ToInt : base64ToInt);
-		int sLen = s.length();
-		int numGroups = sLen / 4;
-		if (4 * numGroups != sLen)
-			throw new IllegalArgumentException(
-					"String length must be a multiple of four.");
-		int missingBytesInLastGroup = 0;
-		int numFullGroups = numGroups;
-		if (sLen != 0) {
-			if (s.charAt(sLen - 1) == '=') {
-				missingBytesInLastGroup++;
-				numFullGroups--;
+	public static byte[] decode(String base64String) {
+		if (StringUtils.isEmpty(base64String)) {
+			return new byte[0];
+		}
+		int i = 0;
+		String s = base64String.trim();
+		int len = s.length();
+		ByteArrayOutputStream os =
+			new ByteArrayOutputStream(s.length() * 2 / 3 + 1);
+		while (true) {
+			if (i == len) {
+				break;
 			}
-			if (s.charAt(sLen - 2) == '=')
-				missingBytesInLastGroup++;
+			int tri =
+				(decode(s.charAt(i)) << 18)
+				+ (decode(s.charAt(i + 1)) << 12)
+				+ (decode(s.charAt(i + 2)) << 6)
+				+ (decode(s.charAt(i + 3)));
+			os.write((tri >> 16) & 0xFF);
+			if (s.charAt(i + 2) == '=')
+				break;
+			os.write((tri >> 8) & 0xFF);
+			if (s.charAt(i + 3) == '=')
+				break;
+			os.write(tri & 0xFF);
+			i += 4;
 		}
-		byte[] result = new byte[3 * numGroups - missingBytesInLastGroup];
+		return os.toByteArray();
+	}
 
-		// Translate all full groups from base64 to byte array elements
-		int inCursor = 0, outCursor = 0;
-		for (int i = 0; i < numFullGroups; i++) {
-			int ch0 = base64toInt(s.charAt(inCursor++), alphaToInt);
-			int ch1 = base64toInt(s.charAt(inCursor++), alphaToInt);
-			int ch2 = base64toInt(s.charAt(inCursor++), alphaToInt);
-			int ch3 = base64toInt(s.charAt(inCursor++), alphaToInt);
-			result[outCursor++] = (byte) ((ch0 << 2) | (ch1 >> 4));
-			result[outCursor++] = (byte) ((ch1 << 4) | (ch2 >> 2));
-			result[outCursor++] = (byte) ((ch2 << 6) | ch3);
-		}
-
-		// Translate partial group, if present
-		if (missingBytesInLastGroup != 0) {
-			int ch0 = base64toInt(s.charAt(inCursor++), alphaToInt);
-			int ch1 = base64toInt(s.charAt(inCursor++), alphaToInt);
-			result[outCursor++] = (byte) ((ch0 << 2) | (ch1 >> 4));
-
-			if (missingBytesInLastGroup == 1) {
-				int ch2 = base64toInt(s.charAt(inCursor++), alphaToInt);
-				result[outCursor++] = (byte) ((ch1 << 4) | (ch2 >> 2));
+	private static int decode(char c) {
+		if (c >= 'A' && c <= 'Z') {
+			return ((int)c) - 65;
+		} else if (c >= 'a' && c <= 'z') {
+			return ((int)c) - 97 + 26;
+		} else if (c >= '0' && c <= '9') {
+			return ((int)c) - 48 + 26 + 26;
+		} else {
+			switch (c) {
+			case '+' :
+				return 62;
+			case '/' :
+				return 63;
+			case '=' :
+				return 0;
+			default :
+				throw new RuntimeException(
+						"解析不可能 char = " + c);
 			}
 		}
-		// assert inCursor == s.length()-missingBytesInLastGroup;
-		// assert outCursor == result.length;
-		return result;
 	}
-
-	private static int base64toInt(char c, byte[] alphaToInt) {
-		int result = alphaToInt[c];
-		if (result < 0)
-			throw new IllegalArgumentException("Illegal character " + c);
-		return result;
-	}
-
-	private static final byte base64ToInt[] = { -1, -1, -1, -1, -1, -1, -1, -1,
-			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			-1, 62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1,
-			-1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-			13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1,
-			-1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
-			41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
-
-	private static final byte altBase64ToInt[] = { -1, -1, -1, -1, -1, -1, -1,
-			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			-1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, -1,
-			62, 9, 10, 11, -1, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 12, 13,
-			14, -1, 15, 63, 16, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 17, -1, 18,
-			19, 21, 20, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-			40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 22, 23, 24, 25 };
 
 }
